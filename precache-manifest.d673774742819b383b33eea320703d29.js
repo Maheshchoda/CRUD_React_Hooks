4bea8fd36c6c2c08543a8f21691d27986fec0862
Maheshchoda <choda.maheshbabu@gmail.com>
2018-12-21 15:06:33 +0530

    Updates

---
+++ precache-manifest.d673774742819b383b33eea320703d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ee6756f6834c16b78a1",
+    "url": "/CRUD_React_Hooks/static/js/runtime~main.0ee6756f.js"
+  },
+  {
+    "revision": "60fcafa6d699d3ab547f",
+    "url": "/CRUD_React_Hooks/static/js/main.60fcafa6.chunk.js"
+  },
+  {
+    "revision": "70954ada4eb7f0a687c5",
+    "url": "/CRUD_React_Hooks/static/js/1.70954ada.chunk.js"
+  },
+  {
+    "revision": "60fcafa6d699d3ab547f",
+    "url": "/CRUD_React_Hooks/static/css/main.2019510e.chunk.css"
+  },
+  {
+    "revision": "70954ada4eb7f0a687c5",
+    "url": "/CRUD_React_Hooks/static/css/1.e93fecdd.chunk.css"
+  },
+  {
+    "revision": "f1d461cc2c44e6d6ca790c21e4b607a1",
+    "url": "/CRUD_React_Hooks/index.html"
+  }
+];
